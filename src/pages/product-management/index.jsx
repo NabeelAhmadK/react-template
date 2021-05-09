d782e5207d91a9efe8b63e9fce7ce83c1a072675
Nabeel Ahmad Khan <nabeel.ahmed@odinesolutions.com>
2021-05-09 19:41:33 +0500

    Pages Skeleton

---
+++ src/pages/product-management/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ProductManagement = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default ProductManagement
---
+++ src/pages/user-management/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const UserManagement = (props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default UserManagement
