f133fa10f9467c64c35879ee9b0e5ed19d34e1e0
Nabeel Ahmad Khan <nabeel.ahmed@odinesolutions.com>
2021-05-09 19:41:23 +0500

    Generic Skeleton

---
+++ src/components/button/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const button = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+button.propTypes = {
+
+}
+
+export default button
---
+++ src/components/input/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const input = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+input.propTypes = {
+
+}
+
+export default input
